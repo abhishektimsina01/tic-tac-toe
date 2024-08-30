2436e5379d873cbdaab2b0eeaf8b0314c88749e0
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-08-31 00:14:02 +0545

    hello

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 const options = ["X's" ,"O's"];
-function ask_for_option(){
-    choice  = prompt("do u want to choose X or O");
-    if(choice == "X"){
-        image = ''
-    }
-}
+
 function start(){
+    
     ask_for_option();
 }
 
-//for child one image
-const child1_img = document.querySelector('one');
-child1_img.src = `${image}.jpg`
+function play(){
+    console.log("Playing bitch");
+}
+
+//popup for option selection
+function ask_for_option(){
+    function ask_for_option(){
+        choice  = prompt("do u want to choose X or O");
+        if(choice == "X"){
+            image = ''
+        }
+    }
+}
+
+//for selection
+function popup(){
+
+}
+
+const child1 = document.querySelector('.one');
+const child2 = document.querySelector('.two');
+const child3 = document.querySelector('.three');
+const child4 = document.querySelector('.four');
+const child5 = document.querySelector('.five');
+const child6 = document.querySelector('.six');
+const child7 = document.querySelector('.seven');
+const child8 = document.querySelector('.eight');
+const child9 = document.querySelector('.nine');
+
+function for_user(){
+
+}
+
+function show(){
+    const s = document.querySelector(".option");
+    s.classList.toggle("option_show");
+}
+
+function close(){
+    // const s = document.querySelector(".option_show");
+    // s.style.display = none;
+    console.log("clicked");
+}
