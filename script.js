bdd403744d64ecc08dc415cec45824d0069e1194
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-08-31 20:25:35 +0545

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //in order to check the flag whether the box is filled ot not
 const box = [1,2,3,4,5,6,7,8,9];
+const child1 = document.getElementById('one');
+console.log(child1);
 
 var is_selected = false;
 function for_cpu(human1_choice){ 
@@ -13,16 +15,32 @@ function for_cpu(human1_choice){
     }
 }
 
-//accessing the boxes
-const child1 = document.querySelector('.one');
-const child2 = document.querySelector('.two');
-const child3 = document.querySelector('.three');
-const child4 = document.querySelector('.four');
-const child5 = document.querySelector('.five');
-const child6 = document.querySelector('.six');
-const child7 = document.querySelector('.seven');
-const child8 = document.querySelector('.eight');
-const child9 = document.querySelector('.nine');
+// //accessing the boxes
+// child1.addEventListener('click', for_img);
+
+// const child2 = document.querySelector('.two');
+// child2.addEventListener('click', for_img);
+
+// const child3 = document.querySelector('.three');
+// child3.addEventListener('click', for_img);
+
+// const child4 = document.querySelector('.four');
+// child4.addEventListener('click', for_img);
+
+// const child5 = document.querySelector('.five');
+// child5.addEventListener('click', for_img);
+
+// const child6 = document.querySelector('.six');
+// child6.addEventListener('click', for_img);
+
+// const child7 = document.querySelector('.seven');
+// child7.addEventListener('click', for_img);
+
+// const child8 = document.querySelector('.eight');
+// child8.addEventListener('click', for_img);
+
+// const child9 = document.querySelector('.nine');
+// child9.addEventListener('click', for_img);
 
 function clear(){
     is_selected = false;
@@ -34,6 +52,7 @@ function play(){
     parent_of_s.classList.add("opt_cont");
     parent_of_s.classList.remove("parent");
     clear();
+    console.log(is_selected);   
 }
 
 function X(){
@@ -42,6 +61,7 @@ function X(){
     parent_of_s.classList.add("parent");
     var human1_choice = "X";
     var is_selected = true;
+    console.log(is_selected);
     console.log(`you choose ${human1_choice}.jpg`);
     for_cpu();
 }
@@ -56,8 +76,9 @@ function O(){
     console.log(`you choose ${human1_choice}.jpg`);
 }
 
-function for_img(x){
-    const y = x.appendChild(child_img);
-    y.src = `${human1_choice}.jpg`
+function for_img(){
+    // var child_img = document.createElement('img');
+    // x.appendChild(child_img);
+    // y.src = `${human1_choice}.jpg`
+    console.log("clicked");  
 }
-
