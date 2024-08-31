ad7308219eb21eb0b2cfd0958a1a6d4a8ab548b9
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-08-31 10:04:43 +0545

    hello

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-const options = ["X's" ,"O's"];
-
-function start(){
-    
-    ask_for_option();
-}
-
-function play(){
-    console.log("Playing bitch");
-}
+const options = ["X" ,"O"];
 
 
-
-//for selection
-function popup(){
-
-}
-
+//accessing the boxes
 const child1 = document.querySelector('.one');
 const child2 = document.querySelector('.two');
 const child3 = document.querySelector('.three');
@@ -26,25 +12,31 @@ const child7 = document.querySelector('.seven');
 const child8 = document.querySelector('.eight');
 const child9 = document.querySelector('.nine');
 
-function for_user(){
-
+function clear(){
+    choice = undefined;
+    
 }
 
 function play(){
     const parent_of_s = document.querySelector(".parent");
     parent_of_s.classList.add("opt_cont");
     parent_of_s.classList.remove("parent");
+    clear();
 }
 
 function X(){
     const parent_of_s = document.querySelector(".opt_cont");
     parent_of_s.classList.remove("opt_cont");
     parent_of_s.classList.add("parent");
+    var choice = "X";
+    console.log("you choose X");
 }
 
 function O(){
     const parent_of_s = document.querySelector(".opt_cont");
     parent_of_s.classList.remove("opt_cont");
     parent_of_s.classList.add("parent");
+    var choice = "O";
+    console.log("you choose O");
 }
 
